3d366e50143373a4c1e0a01c5700d35c6353b7f8
Terri Mitchell <terrimitchell@yahoo.com>
2012-12-02 20:39:20 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omr::Application.config.secret_token = 'b55066cabedf09b7bdc304b02d9ab03e418a74f17d19a17f58927e90ff6b0d0ef3ae8941fb6a8cb140bd8eb6162739822b1565abe58801ef5e3b877324023820'
