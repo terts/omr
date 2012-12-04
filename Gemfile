3829e03b6a082a62e13fea97ad42b45672c7619c
Terri Mitchell <terrimitchell@yahoo.com>
2012-12-03 21:47:04 -0500

    finished sample

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.1'
+gem 'bootstrap-sass'
+gem 'devise'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Omr::Application.routes.draw do
+  devise_for :users
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'static_pages#home'
